c8fb03779c0aa422a5bf434645a0a67bd2ba6a5a
LizaEsina <135700831+LizaEsina@users.noreply.github.com>
2024-01-27 11:13:52 +0300

    1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,76 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    var secretNumber = generateSecretNumber()
+    var attempts = 0
+
+    println("Игра 'Быки и Коровы' начинается!")
+    println("Угадайте секретное число.")
+
+    do {
+        attempts++
+        println("Попытка № $attempts")
+        val guess = readGuessNumber()
+
+        val (bulls, cows) = calculateBullsAndCows(guess, secretNumber)
+        println("Быки: $bulls, Коровы: $cows")
+
+        if (bulls == 4) {
+            println("Вы победили! Загаданное число: $secretNumber")
+            return
+        }
+
+        if (attempts == 10) {
+            println("Вы использовали все попытки.")
+            println("Хотите продолжить игру? (Да/Нет)")
+            val choice = readLine()
+
+            if (choice == "Да") {
+                attempts = 0
+                secretNumber = generateSecretNumber()
+                println("Новая игра начинается!")
+            } else {
+                println("Вы сдались. Загаданное число: $secretNumber")
+                return
+            }
+        }
+
+    } while (true)
+}
+
+fun generateSecretNumber(): String {
+    val digits = mutableListOf<Int>()
+    while (digits.size < 4) {
+        val digit = Random.nextInt(10)
+        if (digits.isEmpty() && digit == 0) {
+            continue
+        } else if (digit !in digits) {
+            digits.add(digit)
+        }
+    }
+    return digits.joinToString("")
+}
+
+fun readGuessNumber(): String {
+    println("Введите четырёхзначное число:")
+    var guess = readLine()
+    while (guess == null || guess.length != 4 || !guess.matches(Regex("\\d+"))) {
+        println("Некорректный ввод. Попробуйте ввести четырёхзначное число:")
+        guess = readLine()
+    }
+    return guess
+}
+
+fun calculateBullsAndCows(guess: String, secret: String): Pair<Int, Int> {
+    var bulls = 0
+    var cows = 0
+
+    for (i in guess.indices) {
+        if (guess[i] == secret[i]) {
+            bulls++
+        } else if (guess[i] in secret) {
+            cows++
+        }
+    }
+    return bulls to cows
 }
